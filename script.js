2a9c0a94a162b378d0d1665c56e6ec88d86470d8
gustavoLVJardim <utilizador@Mac-mini-de-utilizador.local>
2025-01-05 17:01:59 +0000

    completing js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const firstNumber = document.querySelector('#first-number');
 const operator = document.querySelector('#operator');
 const secondNumber = document.querySelector('#second-number');
 
-const result = document.querySelector('#result');
+const result = document.querySelector('#getResult');
 
 let firstNum = '';
 let operatorSign = '';
@@ -28,6 +28,9 @@ function getMulti(num1, num2) {
 }
 
 function getDiv(num1, num2) {
+    if (num2 === 0) {
+        return "Can't div by 0";
+    }
     return num1 / num2;
 }
 
@@ -46,55 +49,52 @@ function operate(num1, num2, operator) {
 
 document.addEventListener('DOMContentLoaded', () => {
     display.textContent = '0'
+    let operatorClicked = false;
     
     numberButtons.forEach(button => {
         button.addEventListener('click', () => {
-            if (display.textContent === '0') {
+            if (display.textContent === '0' || operatorCliked) {
                 display.textContent = '';
+                operatorCliked = false;
             }
             display.textContent += button.textContent;
+            
+            if (!operatorSign) {
+                firstNum = Number(display.textContent);
+                console.log(`primeiro: ${firstNum}`)
+            } else {
+                secondNum = Number(display.textContent)
+                console.log(`segundo: ${secondNum}`)
+            }
         });
     });
 
 
     operatorButtons.forEach(button => {
         button.addEventListener('click', () => {
-            
-            if (firstNum === '') {
-                firstNum = Number(display.textContent);
-                operatorSign = button.textContent;
+            if (firstNum && !operatorClicked) {
+                operatorSign = button.textContent; // Registra o operador
+                operatorClicked = true; // Sinaliza que o operador foi clicado
+                console.log(`operador: ${operatorSign}`);
             }
-                
-                console.log(firstNum)
-                console.log(operatorSign)
-            
-               
-            
-            if (secondNum === '') {
-                display.textContent = firstNum;
-                button.textContent = ''
-                secondNum += button.textContent;
-                console.log(secondNum)
-            }
+        });
+    });
 
-            
+    result.addEventListener('click', () => {
+        if (firstNum && operatorSign && secondNum) {
+            resultNum = operate(firstNum, secondNum, operatorSign);
+            display.textContent = resultNum;
 
-            
-            
-                /*firstNumber.textContent = firstNum;
-                operator.textContent = operatorSign;*/
-        
-            
-        })
-    })
+            // Prepara para nova operação
+            firstNum = resultNum;
+            operatorSign = '';
+            secondNum = '';
+        }
+    });
 
 
     
-    
-    
-    
-    
-    
+
     clean.addEventListener('click', () => {
         display.textContent = '0';
         firstNum = '';
